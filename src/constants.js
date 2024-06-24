4f9aba87c94c4ad97d6e9c81d9befef45a6d2200
ascharyasoni2807 <65384919+ascharyasoni2807@users.noreply.github.com>
2024-06-25 01:43:14 +0530

    added comments

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const NO_IMAGE_URL =
   "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg";
 
+// keeping here as of now, should be kept in env variables
 export const TMDB_API_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2EzMGU1ZTdjMGYzMjY0ODNhMmU2MTRiNjExMjZkNyIsIm5iZiI6MTcxOTEyNzU0OC42MTgzNTEsInN1YiI6IjY2NzdjY2ZmYjY1ZGRiZjNhYTczMjI3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N7MzL6NzaWpfAl7lIlDB6tc9uMWAUzEL4jQ6wZfNnFI";
